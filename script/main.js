183d43b08aca376d0492c8bb477c618ec50b3186
Ubuntu <ubuntu@ip-172-31-89-41.ec2.internal>
2020-06-10 04:49:59 +0000

    Pre 2da entrega

---
+++ script/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('.header').height($(window).height());
+    
+   })
+
+// object-fit polyfill run
+objectFitImages();
+
+/* init Jarallax */
+jarallax(document.querySelectorAll('.jarallax'));
+
+jarallax(document.querySelectorAll('.jarallax-keep-img'), {
+    keepImg: true,
+});
+
+$("#btnLogin").click(function(event) {
+
+    //Fetch form to apply custom Bootstrap validation
+    var form = $("#formLogin")
+
+    if (form[0].checkValidity() === false) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+    
+    form.addClass('was-validated');
+  });
